52476b78d821f9984a63e677ee8e7ce12c038719
Balaji M <159120184+05Balaji@users.noreply.github.com>
2026-06-22 20:15:24 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.title("Real and Fake Image Classification")
+st.write("This is a simple web application to classify images as real or fake using a pre-trained model.")
+st.divider()
+st.sidebar.header("upload the Image") 
